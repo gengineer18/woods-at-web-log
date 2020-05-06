484c0beeccf65453981ba445257b24245254cd1a
妹尾弦 <gengineer.web@gmail.com>
2020-05-07 00:01:48 +0900

    fix: 不要dataの削除

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer fixed app>
       <span>&copy; 2020 Woods At Web</span>
     </v-footer>
   </v-app>
@@ -18,29 +18,6 @@ import TheHeader from '@/components/Organisms/TheHeader.vue'
 export default Vue.extend({
   components: {
     TheHeader,
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify',
-    }
   },
 })
 </script>
