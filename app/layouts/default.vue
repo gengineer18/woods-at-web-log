b0f202eb30add01dc3006fdce7a4bfc619c54af5
妹尾弦 <gengineer.web@gmail.com>
2020-05-07 23:49:12 +0900

    feat: レイアウト変更

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,14 @@
     <the-header />
     <v-content>
       <v-container>
-        <nuxt />
+        <v-row justify="center">
+          <v-col cols="12" sm="12" md="8" xl="6">
+            <nuxt />
+          </v-col>
+          <v-col cols="12" sm="12" md="4" xl="3">
+            <the-aside />
+          </v-col>
+        </v-row>
       </v-container>
     </v-content>
     <v-footer fixed app>
@@ -15,10 +22,14 @@
 <script lang="ts">
 import Vue from 'vue'
 const TheHeader = () => import('@/components/Organisms/TheHeader.vue')
+const TheAside = () => import('@/components/Organisms/TheAside.vue')
 
 export default Vue.extend({
   components: {
     TheHeader,
+    TheAside,
   },
 })
 </script>
+
+<style lang="scss" scoped></style>
