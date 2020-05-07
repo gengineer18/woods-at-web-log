56e9b7082be472e5e37f928deb98806897f70aff
妹尾弦 <gengineer.web@gmail.com>
2020-05-07 23:49:24 +0900

    add: asideコンポーネント追加

---
+++ app/components/Atoms/AsideTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>
+    {{ title }}
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ app/components/Morecules/AsideHeadline.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="aside-headline py-4">
+    <aside-title :title="title" class="text-center" />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+const AsideTitle = () => import('@/components/Atoms/AsideTitle.vue')
+
+export default Vue.extend({
+  components: {
+    AsideTitle,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.aside-headline {
+  background-color: $c-blue;
+  color: $c-white;
+}
+</style>
