a6278c044589bea143fc3f6634b77ee244dc32f8
妹尾弦 <gengineer.web@gmail.com>
2020-05-08 17:16:01 +0900

    add: 記事一覧タイトルの追加

---
+++ app/components/Atoms/CardTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card-title>{{ title }}</v-card-title>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
