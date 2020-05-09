853faa708587be7a55b2159c0c2a24df8b581296
妹尾弦 <gengineer.web@gmail.com>
2020-05-09 12:50:41 +0900

    fix: v-card-textを付与

--- app/components/Atoms/CardTag.vue
+++ app/components/Atoms/CardTag.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-chip :to="to" small label dark color="#e5012c" class="ma-1">
-    <v-icon left size="18" color="white">
-      mdi-tag
-    </v-icon>
-    <span class="font-weight-bold">{{ name }}</span>
-  </v-chip>
+  <v-card-text>
+    <v-chip :to="to" small label dark color="#e5012c" class="ma-1">
+      <v-icon left size="18" color="white">
+        mdi-tag
+      </v-icon>
+      <span class="font-weight-bold">{{ name }}</span>
+    </v-chip>
+  </v-card-text>
 </template>
 
 <script lang="ts">
