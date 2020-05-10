91bd79c790683c2a266f0553c4d715f1527414ea
妹尾弦 <gengineer.web@gmail.com>
2020-05-11 01:25:04 +0900

    feat: サイドナビタグ一覧の追加

--- app/components/Organisms/AsideTag.vue
+++ app/components/Organisms/AsideTag.vue
@@ -1,20 +1,20 @@
 <template>
   <v-card>
     <aside-headline :title="title" :icon="icon" />
-    <p>test</p>
-    <p>test</p>
-    <p>test</p>
-    <p>test</p>
+    <aside-tag-list :tags="tags" />
   </v-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState } from 'vuex'
 const AsideHeadline = () => import('@/components/Morecules/AsideHeadline.vue')
+const AsideTagList = () => import('@/components/Morecules/AsideTagList.vue')
 
 export default Vue.extend({
   components: {
     AsideHeadline,
+    AsideTagList,
   },
   computed: {
     title(): string {
@@ -23,6 +23,7 @@ export default Vue.extend({
     icon(): string {
       return 'mdi-tag'
     },
+    ...mapState(['tags']),
   },
 })
 </script>
