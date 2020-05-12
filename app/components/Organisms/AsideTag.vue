a0c93c8d425a979b26ee2e27232c3784c12d6b66
妹尾弦 <gengineer.web@gmail.com>
2020-05-13 02:01:16 +0900

    feat: タグの件数取得

--- app/components/Organisms/AsideTag.vue
+++ app/components/Organisms/AsideTag.vue
@@ -4,7 +4,11 @@
     <ul class="px-6 py-2">
       <li v-for="(tag, i) in tags" :key="i" class="my-3">
         <nuxt-link :to="link(tag.fields.slug)" class="post-list">
-          <aside-each-list-item :name="tag.fields.name" :icon="icon" />
+          <aside-each-list-item
+            :name="tag.fields.name"
+            :icon="icon"
+            :count="relatedPostsCount(tag, posts)"
+          />
         </nuxt-link>
       </li>
     </ul>
@@ -14,6 +18,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
+import { Post, Tag } from '@/types/common.ts'
 const AsideHeadline = () => import('@/components/Molecules/AsideHeadline.vue')
 const AsideEachListItem = () =>
   import('@/components/Atoms/AsideEachListItem.vue')
@@ -33,6 +38,22 @@ export default Vue.extend({
     link() {
       return function (slug: string) {
         return `/tags/${slug}`
+      }
+    },
+    relatedPostsCount(): (tag: Tag, posts: Post[]) => number {
+      return function (tag: Tag, posts: Post[]): number {
+        let length = 0
+        const postTags = posts.map((post: Post) => post.fields.tags)
+        for (let i = 0; i < postTags.length; i++) {
+          const Tags = postTags[i]
+          // postにタグが付与されていないものはundifinedになるため制御
+          if (Tags) {
+            for (let j = 0; j < Tags.length; j++) {
+              if (Tags[j].sys.id === tag.sys.id) length++
+            }
+          }
+        }
+        return length
       }
     },
     ...mapState(['tags', 'posts']),
