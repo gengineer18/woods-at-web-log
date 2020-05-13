6a71dfb0d516dd53017cfeeeb0ca40e0e723b7e6
妹尾弦 <gengineer.web@gmail.com>
2020-05-13 23:23:59 +0900

    feat: カテゴリーページ整理

--- app/pages/categories/_slug.vue
+++ app/pages/categories/_slug.vue
@@ -1,33 +1,47 @@
 <template>
-  <div>
-    <breadcrumbs />
-    <h1>{{ category.fields.name }}</h1>
-    <div v-for="(post, i) in relatedPosts" :key="i">
-      {{ post.fields.category.fields.name }},
-      {{ post.fields.title }}
-    </div>
-  </div>
+  <v-container fluid>
+    <list-headline
+      :headline="category.fields.name"
+      icon="mdi-folder"
+      class="text-center"
+    />
+    <v-row justify="center">
+      <v-col cols="12" sm="11" md="11">
+        <v-row v-if="relatedPosts.length">
+          <v-col v-for="(post, i) in relatedPosts" :key="i" cols="12" sm="6">
+            <card-flame :post="post" />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import { Post, Category } from '@/types/common.ts'
+
+const ListHeadline = () => import('@/components/Atoms/ListHeadline.vue')
+const CardFlame = () => import('@/components/Organisms/CardFlame.vue')
+
+export default Vue.extend({
+  components: {
+    ListHeadline,
+    CardFlame,
+  },
   async asyncData({ payload, store, params, error }) {
-    const category =
+    const category: Category =
       payload ||
       (await store.state.categories.find(
-        (category) => category.fields.slug === params.slug
+        (category: Category) => category.fields.slug === params.slug
       ))
 
     if (category) {
-      return { category }
+      const relatedPosts: Post[] = store.getters.relatedPosts(category)
+      return { category, relatedPosts }
     } else {
       return error({ statusCode: 400 })
     }
   },
-  computed: {
-    relatedPosts() {
-      return this.$store.getters.relatedPosts(this.category)
-    },
-  },
-}
+})
 </script>
