62ce1f686e474de228bbb8f611c235a93b9dbdd9
妹尾弦 <gengineer.web@gmail.com>
2020-05-15 00:31:56 +0900

    feat: タグにリンクを追加

--- app/components/Organisms/ArticleCategoryAndTag.vue
+++ app/components/Organisms/ArticleCategoryAndTag.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <template v-if="post.fields.category">
-      <card-category
-        :name="post.fields.category.fields.name"
-        class="category-and-tags px-0 mb-2"
-      />
+      <nuxt-link :to="categoryLink(post)">
+        <card-category
+          :name="post.fields.category.fields.name"
+          class="category-and-tags px-0 mb-2"
+        />
+      </nuxt-link>
     </template>
     <template v-for="tag in post.fields.tags">
-      <card-tag :key="tag.sys.id" :name="tag.fields.name" class="tags" />
+      <nuxt-link :key="tag.sys.id" :to="tagLink(tag)">
+        <card-tag :name="tag.fields.name" class="tags" />
+      </nuxt-link>
     </template>
   </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { Post } from '@/types/common'
+import { Post, Tag } from '@/types/common'
 
 const CardTag = () => import('@/components/Atoms/CardTag.vue')
 const CardCategory = () => import('@/components/Atoms/CardCategory.vue')
@@ -28,6 +32,18 @@ export default Vue.extend({
     post: {
       type: Object as PropType<Post>,
       required: true,
+    },
+  },
+  computed: {
+    categoryLink(): (post: Post) => string {
+      return function (post: Post): string {
+        return `/categories/${post.fields.category.fields.slug}`
+      }
+    },
+    tagLink(): (tag: Tag) => string {
+      return function (tag: Tag): string {
+        return `/tags/${tag.fields.slug}`
+      }
     },
   },
 })
