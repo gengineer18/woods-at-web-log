38345361ed86269bf7236025b16ae96e4a05b098
妹尾弦 <gengineer.web@gmail.com>
2020-05-17 17:30:18 +0900

    feat: githubアイコンの追加

--- app/components/Atoms/IconTwitter.vue
+++ app/components/Atoms/IconTwitter.vue
@@ -1,28 +1,12 @@
 <template>
-  <figure>
-    <img
-      src="~assets/images/twitter.png"
-      alt="twitter"
-      :width="width"
-      :height="height"
-    />
-  </figure>
+  <a href="https://twitter.com/gengineer18" target="_blank">
+    <v-icon x-large color="#4099ff">mdi-twitter</v-icon>
+  </a>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  props: {
-    width: {
-      type: Number,
-      required: true,
-    },
-    height: {
-      type: Number,
-      required: true,
-    },
-  },
-})
+export default Vue.extend({})
 </script>
 
 <style lang="scss" scoped></style>
